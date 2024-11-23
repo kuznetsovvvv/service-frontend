0380f80c5a7cf9d796d7378e115b0dbdfe65009b
kuznetsovvvv <mihail-160505@mail.ru>
2024-11-23 15:28:45 +0200

    third fix commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,167 +1,247 @@
 <script>
 	import { onMount } from 'svelte';
+	import { goto } from '$app/navigation';
+	let courier_age = '';
+	let courier_gender = '';
+	let courier_phone = '';
+	let courier_active = '';
+	let courier_errorMessage = '';
+
+	function courier_validateForm() {
+		courier_errorMessage = ''; // Очищаем сообщение об ошибке
+
+		if (isNaN(parseInt(courier_age)) || parseInt(courier_age) <= 0) {
+			courier_errorMessage = 'Age must be a positive number.';
+			return false;
+		}
+
+		const phoneRegex = /^[+]?\d{11,12}$/;
+		if (!phoneRegex.test(courier_phone)) {
+			courier_errorMessage = 'Phone number must be 11 or 12 digits long.';
+			return false;
+		}
+
+		return true;
+	}
+
+	async function courier_handleSubmit() {
+		if (!courier_validateForm()) return; // Прерываем отправку, если форма не прошла валидацию
+
+		const courierData = {
+			age: parseInt(courier_age),
+			gender: courier_gender,
+			phone: courier_phone,
+			active: courier_active
+		};
+
+		try {
+			const response = await fetch('http://localhost:18080/api/couriers', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify(courierData)
+			});
+
+			if (!response.ok) {
+				const errorData = await response.json();
+				courier_errorMessage = errorData.message || `HTTP error! status: ${response.status}`;
+			} else {
+				alert('Courier added successfully!');
+				courier_age = '';
+				courier_gender = '';
+				courier_phone = '';
+				courier_active = '';
+			}
+		} catch (error) {
+			console.error('Error adding courier:', error);
+			courier_errorMessage = 'An unexpected error occurred.';
+		}
+	}
+
+    let update_courier_id = '';
+	let update_courier_active = '';
+	let update_error_message = '';
+
+	function update_courier_validateForm() {
+		update_error_message = ''; // Очищаем сообщение об ошибке
+
+		if (isNaN(parseInt(update_courier_id)) || parseInt(update_courier_id) <= 0) {
+			update_error_message = 'Courier id must be a positive number.';
+			return false;
+		}
+		return true;
+	}
+
+	async function update_courier_status() {
+		if (!update_courier_validateForm()) return;
+
+		const updateData = {
+			courier_id: parseInt(update_courier_id),
+			active: update_courier_active
+		};
+		try {
+			const response = await fetch('http://localhost:18080/api/v1/couriers:id', {
+				method: 'DELETE',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify(updateData)
+			});
+			if (!response.ok) {
+				const errorData = await response.json();
+				update_error_message = errorData.message || `HTTP error! status: ${response.status}`;
+			} else {
+				alert('Courier status updated successfully!');
+				update_courier_id = '';
+				update_courier_active = '';
+			}
+		} catch (error) {
+			console.error('Error adding user:', error);
+			update_error_message = 'An unexpected error occurred.';
+		}
+	}
 
 
-  let age = '';
-  let gender = '';
-  let phone = '';
-  let active = '';
-  let errorMessage = '';
-
-  function validateForm() {
-    errorMessage = ''; // Очищаем сообщение об ошибке
-
-    if (isNaN(parseInt(age)) || parseInt(age) <= 0) {
-      errorMessage = 'Age must be a positive number.';
-      return false;
-    }
-
-    const phoneRegex = /^\d{11,12}$/;
-    if (!phoneRegex.test(phone)) {
-      errorMessage = 'Phone number must be 11 or 12 digits long.';
-      return false;
-    }
-
-    return true;
-  }
-
-  async function handleSubmit() {
-    if (!validateForm()) return; // Прерываем отправку, если форма не прошла валидацию
-
-    const courierData = {
-      id: crypto.randomUUID(),
-      age: parseInt(age),
-      gender: gender,
-      phone: phone,
-      active: active
-    };
-
-    try {
-      const response = await fetch('http://localhost:18080/api/couriers', {
-         method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(courierData)
-      });
-
-      if (!response.ok) {
-        const errorData = await response.json();
-        errorMessage = errorData.message || `HTTP error! status: ${response.status}`;
-      } else {
-        alert('Courier added successfully!');
-        age = '';
-        gender = '';
-        phone = '';
-        active = '';
-      }
-    } catch (error) {
-      console.error('Error adding courier:', error);
-      errorMessage = 'An unexpected error occurred.';
-    }
-  }
 
 
 
 
 
+	let user_age = '';
+	let user_gender = '';
+	let user_phone = '';
+	let user_errorMessage = '';
 
-// 	onMount(() => {
-        
-// fetch('http://localhost:18080/api/couriers')
-//   .then((response) => {
-//     return response.text();
-//   })
-//   .then((data) => {
-//     suggestions=data
-//     console.log(data);
-//   });
-// 	console.log('the component has mounted');
-//   });
+	function user_validateForm() {
+		user_errorMessage = ''; // Очищаем сообщение об ошибке
+
+		if (isNaN(parseInt(user_age)) || parseInt(user_age) <= 0) {
+			user_errorMessage = 'Age must be a positive number.';
+			return false;
+		}
+
+		const phoneRegex = /^[+]?\d{11,12}$/;
+		if (!phoneRegex.test(user_phone)) {
+			user_errorMessage = 'Phone number must be 11 or 12 digits long.';
+			return false;
+		}
+
+		return true;
+	}
+
+	async function user_handleSubmit() {
+		if (!user_validateForm()) return; // Прерываем отправку, если форма не прошла валидацию
+
+		const userData = {
+			age: parseInt(user_age),
+			gender: user_gender,
+			phone: user_phone
+		};
+
+		try {
+			const response = await fetch('http://localhost:18080/api/v1/register', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify(userData)
+			});
+
+			if (!response.ok) {
+				const errorData = await response.json();
+				user_errorMessage = errorData.message || `HTTP error! status: ${response.status}`;
+			} else {
+				alert('User added successfully!');
+				user_age = '';
+				user_gender = '';
+				user_phone = '';
+			}
+		} catch (error) {
+			console.error('Error adding user:', error);
+			user_errorMessage = 'An unexpected error occurred.';
+		}
+	}
 
 
-  
+
+	let user_phone_info = '';
+	let user_errorMessage_info = '';
+  	let user_data=null;
+	function infovalidateForm() {
+		user_errorMessage_info = ''; // Очищаем сообщение об ошибке
+
+		const phoneRegex = /^[+]?\d{11,12}$/;
+		if (!phoneRegex.test(user_phone_info)) {
+			user_errorMessage_info = 'Phone number must be 11 or 12 digits long.';
+			return false;
+		}
+
+		return true;
+	}
+  async function infohandleSubmit() {
+		if (!infovalidateForm()) return; // Прерываем отправку, если форма не прошла валидацию
+   
+		try {
+      const params = new URLSearchParams();
+    params.append('phone', user_phone_info);
+
+			const response = await fetch(`http://localhost:18080/api/v1/profile?${params}`);
+
+			if (!response.ok) {
+				const errorData = await response.json();
+				user_errorMessage_info = errorData.message || `HTTP error! status: ${response.status}`;
+
+			} else {
+        user_data = await response.json();
+        user_phone_info = '';
+			}
+		} catch (error) {
+			console.error('Error fetching user info:', error);
+			user_errorMessage_info = 'An unexpected error occurred.';
+      user_data=null;
+		}
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+	//let suggestions;
+	// 	onMount(() => {
+
+	// fetch('http://localhost:18080/api/couriers')
+	//   .then((response) => {
+	//     return response.text();
+	//   })
+	//   .then((data) => {
+	//     suggestions=data
+	//     console.log(data);
+	//   });
+	// 	console.log('the component has mounted');
+	//   });
 </script>
 
-
-
-
-<style>
-  .colored-gray {
-    background-color: #7b7e7b;
-    color: #2c3726; 
-    padding: 8px;
-    border-top: 1px solid rgb(5, 3, 3);
-    border-bottom: 1px solid rgb(5, 3, 3);
-    width: 100%;
-    box-sizing: border-box;
-    margin: 0px auto;
-  }
-
-  .colored-green {
-    background-color: #0dcd17;
-    color: #2c3726; 
-    padding: 25px;
-    border-bottom: 1px solid rgb(5, 3, 3);
-    width: 100%;
-    box-sizing: border-box;
-    margin: 0px auto;
-    display: flex; /* Включаем Flexbox */
-    justify-content: space-between; /* Распределяем элементы по краям */
-    align-items: center; 
-  }
-
-  .header {
-    font-size: 3em;
-    margin: 0;
-    flex-grow: 1;
-    text-align: center;
-  }
-
-  .text1 {
-    font-size: 3em;
-    margin: 0;
-    flex-grow: 1;
-    text-align: center;
-  }
-
-  .text2 {
-    font-size: 1em;
-    margin: 0;
-    flex-grow: 1;
-    text-align: right;
-  }
-
-  .enter-button {
-    padding: 10px 30px; /* Отступы внутри кнопки */
-    background-color: #2c3726; 
-    color: #0dcd17;
-    text-decoration: none; 
-    border-radius: 6px;
-  }
-</style>
-
 <div class="colored-gray">
-  <div class="text2">
-     <p>+79814576088  24-hour room Service</p>
-  </div>
-
+	<div class="text2">
+		<p>+79814576088 24-hour room Service</p>
+	</div>
 </div>
-
-
-
-
 
 <div class="colored-green">
+	<div class="header">
+		<h1>EcoMarket</h1>
+	</div>
 
-  
-  <div class="header">
-    <h1 >EcoMarket</h1>
-  </div>
-
-  <a href="#top" class="enter-button">Enter</a>
+	<a href="#top" class="enter-button">Enter</a>
 </div>
-
 
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
@@ -169,26 +249,90 @@
 
 
 
+<form on:submit|preventDefault={courier_handleSubmit}>
+	<label for="age">Age:</label>
+	<input type="number" id="age" bind:value={courier_age} />
 
+	<label for="gender">Gender:</label>
+	<input type="text" id="gender" bind:value={courier_gender} />
 
-<form on:submit|preventDefault={handleSubmit}>
-  <label for="age">Age:</label>
-  <input type="number" id="age" bind:value={age} />
+	<label for="phone">Phone:</label>
+	<input type="text" id="phone" bind:value={courier_phone} />
 
-  <label for="gender">Gender:</label>
-  <input type="text" id="gender" bind:value={gender} />
+	<label for="active">Active:</label>
+	<input type="text" id="active" bind:value={courier_active} />
 
-  <label for="phone">Phone:</label>
-  <input type="tel" id="phone" bind:value={phone} />
-
-  <label for="active">Active:</label>
-  <input type="text" id="active" bind:value={active} />
-
-  <button type="submit">Add Courier</button>
-  {#if errorMessage}
-    <p style="color: red;">{errorMessage}</p>
-  {/if}
+	<button class="enter-button">Add Courier</button>
+	{#if courier_errorMessage}
+		<p style="color: red;">{courier_errorMessage}</p>
+	{/if}
 </form>
+
+
+<form on:submit|preventDefault={update_courier_status}>  
+	<label for="update_courier_id">Courier id:</label>
+	<input type="number" id="update_courier_id" bind:value={update_courier_id} />
+
+	<label for="update_courier_active">Active:</label>
+	<input type="text" id="update_courier_active" bind:value={update_courier_active} />
+  <button class="enter-button">Update courier</button>
+  {#if update_error_message}
+  <p style="color: red;">{update_error_message}</p>
+{/if}
+</form>
+
+
+<form on:submit|preventDefault={user_handleSubmit}>
+	<label for="age">Age:</label>
+	<input type="number" id="age" bind:value={user_age} />
+
+	<label for="gender">Gender:</label>
+	<input type="text" id="gender" bind:value={user_gender} />
+
+	<label for="phone">Phone:</label>
+	<input type="text" id="phone" bind:value={user_phone} />
+
+	<button class="enter-button">Add User</button>
+	{#if user_errorMessage}
+		<p style="color: red;">{user_errorMessage}</p>
+	{/if}
+</form>
+
+
+
+<form on:submit|preventDefault={infohandleSubmit}>  
+	<label for="user_phone_info">Phone:</label>
+	<input type="number" id="user_phone_info" bind:value={user_phone_info} />
+
+  <button class="enter-button">Get info user</button>
+  {#if user_errorMessage_info}
+  <p style="color: red;">{user_errorMessage_info}</p>
+{/if}
+</form>
+{#if user_data} 
+<p>User ID: {user_data.user_id}, Age: {user_data.age}, Gender: {user_data.gender}, Phone: {user_data.phone}</p>
+{/if}
+
+
+
+
+
+
+
+
+
+<div class="button-container">
+	<button class="enter-button" on:click={() => goto('/basket')}>Go to order</button>
+  </div>
+
+
+
+
+
+
+
+
+
 
 
 
@@ -197,3 +341,94 @@
 
 
 <!-- <img src="http://google.com/coolcat.jpg" /> -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<style>
+	.colored-gray {
+		background-color: #7b7e7b;
+		color: #2c3726;
+		padding: 8px;
+		border-top: 1px solid rgb(5, 3, 3);
+		border-bottom: 1px solid rgb(5, 3, 3);
+		width: 100%;
+		box-sizing: border-box;
+		margin: 0px auto;
+	}
+
+	.colored-green {
+		background-color: #0dcd17;
+		color: #2c3726;
+		padding: 25px;
+		border-bottom: 1px solid rgb(5, 3, 3);
+		width: 100%;
+		box-sizing: border-box;
+		margin: 0px auto;
+		display: flex; /* Включаем Flexbox */
+		justify-content: space-between; /* Распределяем элементы по краям */
+		align-items: center;
+	}
+
+	.header {
+		font-size: 3em;
+		margin: 0;
+		flex-grow: 1;
+		text-align: center;
+	}
+
+	.text1 {
+		font-size: 3em;
+		margin: 0;
+		flex-grow: 1;
+		text-align: center;
+	}
+
+	.text2 {
+		font-size: 1em;
+		margin: 0;
+		flex-grow: 1;
+		text-align: right;
+	}
+
+	.button-container {
+  display: flex;
+  justify-content: flex-end; 
+  margin-bottom: 10px; 
+} 
+
+	.enter-button {
+		padding: 10px 30px; /* Отступы внутри кнопки */
+		background-color: #2c3726;
+		color: #0dcd17;
+		text-decoration: none;
+		border-radius: 6px;
+		margin-right: 10px;
+	}
+</style>
--- src/routes/basket/+page.svelte
+++ src/routes/basket/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+  import { goto } from '$app/navigation';
 let items = [];
 for (let i = 1; i <= 3; i++) {
   items.push(i);
@@ -11,12 +12,52 @@ for (let i = 1; i <= 3; i++) {
 
 
 
-
-
-
-
-
-
+<head>
+  <title>Centered Blocks</title>
+<style>
+  .container {
+    display: flex;
+    justify-content: center; /* Center horizontally */
+    align-items: center;    /* Center vertically */
+    height: 300px;         /* Adjust as needed */
+    width: 80%;            /* Adjust as needed, responsive layout */
+    margin: 0 auto;        /* Center container within page */
+    border: 1px solid #ccc; /* Optional border for visual clarity */
+  }
+  
+  .block {
+    width: 30%;             /* Adjust block width as desired */
+    height: 100%;            /* Ensures equal height */
+    background-color: #f0f0f0; /* Light gray background */
+    margin: 0 1%;            /* Adds some space between blocks */
+    text-align: center;       /* Centers text within block */
+    border: 1px solid #ddd;   /* Optional border */
+    padding: 20px;            /* Add padding around the content */
+    box-sizing: border-box;   /* Important! Includes padding in the width calculation */
+  }
+  
+  </style>
+  </head>
+  <!-- <body>
+  
+  <div class="container">
+    <div class="block">
+      Add your image here -->
+      <!-- <img src="image1.jpg" alt="Image 1" style="max-width: 100%; max-height: 100%; display: block; margin: 0 auto;">  
+    </div>
+    <div class="block">
+      <img src="image2.jpg" alt="Image 2" style="max-width: 100%; max-height: 100%; display: block; margin: 0 auto;">
+    </div>
+    <div class="block">
+      <img src="image3.jpg" alt="Image 3" style="max-width: 100%; max-height: 100%; display: block; margin: 0 auto;">
+    </div>
+  </div>
+  
+  </body> -->
+ 
+  <div class="button-container">
+  <button class="enter-button" on:click={() => goto('/')}>Go back</button>
+  </div>
 
 <h1>Список покупок</h1>
 <ul>
@@ -24,3 +65,26 @@ for (let i = 1; i <= 3; i++) {
     <li>{item}</li>
   {/each}
 </ul>
+
+
+
+
+<style>
+
+.button-container {
+  display: flex;
+  justify-content: flex-end; 
+  margin-bottom: 10px; 
+} 
+
+	.enter-button {
+		padding: 10px 30px; /* Отступы внутри кнопки */
+		background-color: #2c3726;
+		color: #0dcd17;
+		text-decoration: none;
+		border-radius: 6px;
+		margin-right: 10px;
+	}
+
+
+</style>
